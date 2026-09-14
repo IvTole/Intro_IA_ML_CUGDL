54b97c9a7d1af7a75a7498be07a4e92a8e7247f2
IvTole <toledano.ivan16@gmail.com>
2026-09-14 13:28:21 -0600

    add num, categorical, and geo features list

--- 02_ml_modules/src/config.py
+++ 02_ml_modules/src/config.py
@@ -7,6 +7,25 @@ SEED = 42
 TRAIN_DATA_PATH = "data/train.csv"
 TEST_DATA_PATH = "data/test.csv"
 
-# Variables
+# FEATURES
+
+GEO_FEATURES = [
+    "longitude",
+    "latitude"
+]
+
+NUM_FEATURES = [
+    "housing_median_age",
+    "total_rooms",
+    "total_bedrooms",
+    "population",
+    "households",
+    "median_income",
+]
+CAT_FEATURES = ["ocean_proximity"]
+
+FEATURES =  GEO_FEATURES + NUM_FEATURES + CAT_FEATURES
+
+# TARGET
 TARGET = "median_house_value"
 
