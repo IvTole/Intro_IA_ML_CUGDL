6a1f9438c8e9c1c8732813eb316537d77da1f355
IvTole <toledano.ivan16@gmail.com>
2026-09-14 13:31:18 -0600

    add pipeline (preprocessor + model), and training/evaluation from ModelEvaluation class

--- 02_ml_modules/train.py
+++ 02_ml_modules/train.py
@@ -1,17 +1,37 @@
 # PARA EL USUARIO
 
 from src.io import Dataset
+from src.evaluation import ModelEvaluation
+
+# Modelos
+from src.preprocessor import build_preprocessor
+from sklearn.pipeline import Pipeline
+from sklearn.linear_model import LinearRegression
+from sklearn.tree import DecisionTreeRegressor
+from sklearn.ensemble import RandomForestRegressor
+from sklearn.svm import SVR
+from sklearn.neighbors import KNeighborsRegressor
+from sklearn.neural_network import MLPRegressor
+
 
 def train():
 
     # Importacion y preprocesamiento datos
     data = Dataset(seed=43)
 
-    X, y = data.load_xy()
-    print(X)
+    X_train, y_train = data.load_xy()
 
+    # Model pipeline (preprocessing + model, Linear Regression)
+    pipeline_lr = Pipeline(
+        [
+            ("preprocessor", build_preprocessor()),
+            ("model", KNeighborsRegressor())
+        ]
+    )
 
     # Entrenamiento y métricas
+    ev = ModelEvaluation(X=X_train, y=y_train)
+    ev.evaluate_model(pipeline_lr)
 
     return
 
