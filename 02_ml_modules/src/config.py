779e0d915bdbc83b692502d1b8aefd4fac8e7af1
IvTole <toledano.ivan16@gmail.com>
2026-09-14 05:33:33 -0600

    add config.py, for pipeline variables

---
+++ 02_ml_modules/src/config.py
@@ -0,0 +1,12 @@
+# Configuracion de mis variables utilizadas
+
+# General
+SEED = 42
+
+# Path
+TRAIN_DATA_PATH = "data/train.csv"
+TEST_DATA_PATH = "data/test.csv"
+
+# Variables
+TARGET = "median_house_value"
+
